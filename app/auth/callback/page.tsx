6e73b4a1e433432c4bcf086d032265d2da0338e2
asia272 <asiaashraf7272@gmail.com>
2026-07-09 11:53:49 +0500

    first commit

---
+++ app/auth/callback/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>Call back page will be display here</div>
+    )
+}
+
+export default page
---
+++ app/auth/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='flex h-screen w-full'>
+            <div className='flex-1'></div>
+            <div className='flex-1'></div>
+        </div>
+    )
+}
+
+export default page
